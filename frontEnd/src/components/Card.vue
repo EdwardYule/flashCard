5283ef499ec2fbd6e826b187eac38c74240df028
Edward <787322353@qq.com>
2024-05-08 14:17:49 +0800

    ✨ feat: 翻转卡片

---
+++ frontEnd/src/components/Card.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="card" :class="isFlip ? 'flip' : ''" @click="flip">
+    <div class="front">
+      <slot name="front" />
+    </div>
+    <div class="back">
+      <slot name="back" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  data(){
+    return {
+      isFlip: false,
+    }
+  },
+  methods: {
+    flip(){
+      this.isFlip = !this.isFlip;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  perspective: 1000px;
+  width: 320px;
+  height: 480px;
+  transition: 0.6s;
+  transform-style: preserve-3d;
+  position: relative;
+  &.flip{
+    transform: rotateY(180deg);
+  }
+  .front,.back {
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .front{
+    background-color: lightblue;
+    z-index: 1;
+  }
+  .back{
+    background-color: lightcoral;
+    transform: rotateY(180deg);
+  }
+}
+</style>
--- frontEnd/src/views/HomeView.vue
+++ frontEnd/src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card>
+      <template v-slot:front>
+        front
+      </template>
+      <template v-slot:back>
+        back
+      </template>
+    </Card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Card from "@/components/Card.vue";
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Card,
   }
 }
 </script>
