6500daf28ef84820f366c4a10ea9649ad22728a5
Edward <787322353@qq.com>
2024-05-22 14:04:45 +0800

    ✨ feat: 拖拽功能

--- frontEnd/src/plugins/directives.js
+++ frontEnd/src/plugins/directives.js
@@ -2,7 +2,24 @@ export default {
   install(Vue){
     Vue.directive('draggable', {
       inserted(el){
-        el.draggable = true;
+        el.onmousedown = (event) => {
+          const shiftX = event.clientX - el.getBoundingClientRect().left;
+          const shiftY = event.clientY - el.getBoundingClientRect().top;
+          const onMouseMove = (event) => {
+            el.style.left = event.pageX - shiftX + 'px';
+            el.style.top = event.pageY - shiftY + 'px';
+          }
+          el.style.position = 'absolute';
+          el.style.zIndex = 1000;
+          document.body.append(el);
+          onMouseMove(event);
+          document.addEventListener('mousemove', onMouseMove);
+          el.onmouseup = () => {
+            document.removeEventListener('mousemove', onMouseMove);
+            el.onmouseup = null;
+          };
+        };
+        el.ondragstart = () => false;
       }
     })
   }
