6a617ee7dd641ae04fe7395c3b0122a3b7406bc7
Edward Chan <31300219+EdwardYule@users.noreply.github.com>
2024-05-30 11:00:21 +0000

    为在codespace上开发做准备

--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import plugins from './plugins'
+import VConsole from 'vconsole';
+const vConsole = new VConsole({ theme: 'dark' });
 
 Vue.config.productionTip = false
 Vue.use(plugins)
